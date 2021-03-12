bf397025a87406b664313876de4f4ed05a20df61
Jason Lengstorf <jason@lengstorf.com>
2021-03-12 15:02:50 -0800

    feat: redirects testing

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -2,7 +2,6 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     headers: {
-      'X-Header-Test': 'one',
       'X-Header-Test': 'two',
     },
     body: JSON.stringify(event.multiValueQueryStringParameters),
